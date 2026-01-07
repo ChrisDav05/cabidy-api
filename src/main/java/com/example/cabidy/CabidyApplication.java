e213f1bcfca4c8e9b61fbcf7684731a5a1750a8c
ChrisDav05 <davioteotonio05@gmail.com>
2026-01-07 14:33:23 -0300

    first commit

---
+++ src/main/java/com/example/cabidy/CabidyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabidyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cabidy/CabidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
